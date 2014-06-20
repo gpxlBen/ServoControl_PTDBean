3b1bd342ea7f975d2c44747ced4fda423a41700a
Ben Harraway <ben@gourmetpixel.com>
2014-06-20 21:35:25 +0100

    Tidy up

---
+++ ServoControl Bean Code/ServoControl/ServoControl.ino
@@ -0,0 +1,22 @@
+// ServoContrl
+// by Ben Harraway - http://www.gourmetpixel.com
+// A simple demonsration for the LightBlue Bean by Punch Through Design
+// This sketch looks for input into the scratch and moves a servo based on the scratch value
+// This example code is in the public domain.
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+  
+void setup() 
+{ 
+  myservo.attach(3);  // attaches the servo on pin 3 to the servo object 
+} 
+ 
+ 
+void loop() 
+{   
+    uint16_t number = Bean.readScratchNumber(1);
+    myservo.write(number);    
+    Bean.sleep(100);
+}
